adb777b2b1acfc2a4de04efd2223c03248013eb1
OdinPT <leonardo.almeidavieira@gmail.com>
2020-03-11 15:42:31 +0000

    First commit

---
+++ DatingAppD3/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingappD3.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingappD3.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //equivalente a ValuesController
+        private readonly DataContext _context;
+        private readonly ILogger<WeatherForecastController> _logger;
+       
+        //construtor
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetValue(int id)
+        {
+
+            var value = await  _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value); 
+        }
+
+    }
+}
---
+++ DatingAppD3/WeatherForecast.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DatingappD3.API
+{
+    public class WeatherForecast
+    {
+        //public DateTime Date { get; set; }
+
+        //public int TemperatureC { get; set; }
+
+        //public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string value { get; set; }
+
+  
+    }
+}
