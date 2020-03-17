57a208a9b4e025cc5a884d108e89b82fe0225a17
OdinPT <leonardo.almeidavieira@gmail.com>
2020-03-17 16:41:12 +0000

    4.6

--- DatingAPP-SPA/src/app/app-nav/app-nav.component.ts
+++ DatingAPP-SPA/src/app/app-nav/app-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/_services/auth.service';
 
 @Component({
   selector: 'app-app-nav',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-nav.component.css']
 })
 export class AppNavComponent implements OnInit {
+  model : any  = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
-  }
+  } 
 
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in sucessfully');
+    }, error => {
+      console.log('Failed to login');
+    });
+    //console.log(this.model);
+  }
 }
--- DatingAPP-SPA/src/app/app.module.ts
+++ DatingAPP-SPA/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { AppNavComponent } from './app-nav/app-nav.component';
+import { AuthService } from 'src/_services/auth.service';
 
 
 @NgModule({
@@ -17,9 +19,12 @@ import { AppNavComponent } from './app-nav/app-nav.component';
    imports: [
 	 BrowserModule,
 	 AppRoutingModule,
-	 HttpClientModule
+    HttpClientModule ,
+    FormsModule
 	],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
