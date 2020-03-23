813c9c0cfb95b4d45b067d463ab250effbb4dd17
OdinPT <leonardo.almeidavieira@gmail.com>
2020-03-23 23:04:53 +0000

    7.8

--- DatingAppD3/Data/IDatingRepository.cs
+++ DatingAppD3/Data/IDatingRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DatingappD3.API.Data
 {
-    interface IDatingRepository
+    public interface IDatingRepository
     {
         void Add<T>(T entity) where T : class;
         void Delete<T>(T entity) where T : class;
