ad20494806d928fb295582f53566fe5da8446136
OdinPT <leonardo.almeidavieira@gmail.com>
2020-03-25 10:05:20 +0000

    9.6

--- DatingAppD3/Dtos/UserForListDto.cs
+++ DatingAppD3/Dtos/UserForListDto.cs
@@ -17,7 +17,7 @@ namespace DatingappD3.API.Dtos
 
         public int Age { get; set; }
 
-        public string KnownAs { get; set; }
+        public string knownAs { get; set; }
 
         public DateTime Created { get; set; }
 
